3ecae2ba418ad0aa5e780475140a628745bdcead
andresramosarevalo-rgb <andresramosarevalo@gmail.com>
2026-04-22 10:22:52 -0500

    feat: add Exceptions

---
+++ src/main/java/com/taller/bookstore/exception/custom/AuthorHasBooksException.java
@@ -0,0 +1,3 @@
+package com.taller.bookstore.exception.custom;
+
+public class AuthorHasBooksException extends RuntimeException { public AuthorHasBooksException(String message) { super(message); } }
